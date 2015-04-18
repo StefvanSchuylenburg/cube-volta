db4360c44e3737a539650292bd8670a5b29712d7
Stef van Schuylenburg <s.b.v.schuylenburg@student.tue.nl>
2015-04-18 23:09:21 +0200

    Add main menu

---
+++ Assets/scripts/Menu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    //this kills the application
+    public void Exit() 
+    {
+        Application.Quit();
+    }
+
+    public void LoadLevel(string name)
+    {
+        Application.LoadLevel(name);
+    }
+}
