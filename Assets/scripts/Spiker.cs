689b2b287e1130070e675dd19a938542fe2b9757
David Vollmar <david@vollmar.nl>
2015-04-18 16:31:25 +0200

    updated scene with spikes

---
+++ Assets/scripts/Spiker.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spiker : MonoBehaviour {
+
+    public Material deathMaterial;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+    void OnTriggerEnter(Collider other)
+    {
+
+        var body = other.gameObject.GetComponent<Rigidbody>() as Rigidbody;
+        body.isKinematic = true;
+        var ren = other.gameObject.GetComponent<Renderer>() as Renderer;
+        ren.material = deathMaterial;
+    }
+}
