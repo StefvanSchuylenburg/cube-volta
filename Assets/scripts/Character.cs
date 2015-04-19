973b1de3132a2def7cf42875918f2768022ef81e
Stef van Schuylenburg <s.b.v.schuylenburg@student.tue.nl>
2015-04-19 14:40:43 +0200

    Add support for winning and losing
    It is not completely implemented yet, because the Character is not
    implemented.

---
+++ Assets/scripts/Character.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+/**
+ * A block that can be killed.
+ */
+public class Character : MonoBehaviour {
+
+    public bool isAlive
+    {
+        get;
+        private set;
+    }
+
+    /**
+     * Trigger for when the Character becomes dead
+     */
+    public UnityEvent onDeath = new UnityEvent();
+
+    void Awake()
+    {
+        isAlive = true;
+    }
+
+    /**
+     * Kills the Character.
+     * It is now no longer alive.
+     * This does not influence his abilities to move.
+     */
+    public void Kill()
+    {
+        isAlive = false;
+        onDeath.Invoke();
+    }
+}
---
+++ Assets/scripts/GameState.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+using System.Linq;
+
+/**
+ * Keeps track of the state of the game and whether the players and enemies are still alive.
+ */
+public class GameState : MonoBehaviour {
+
+    /**
+     * The characters that all should stay alive.
+     */
+    public Character[] players;
+
+    /**
+     * The character that all should be killed.
+     */
+    public Character[] enemies;
+
+    /**
+     * The player has won this level.
+     */
+    public UnityEvent onWin = new UnityEvent();
+
+    /**
+     * The player has lost the level.
+     */
+    public UnityEvent onGameOver = new UnityEvent();
+
+	// Use this for initialization
+	void Start () {
+        // add calls to CheckGameState() when an event happens to the players/enemies
+        foreach (var c in players.Concat(enemies))
+        {
+            c.onDeath.AddListener(CheckGameState);
+        }
+	}
+
+    /**
+     * This checks whether the game is over.
+     */
+    private void CheckGameState()
+    {
+        // check whether all players are alive
+        var allPlayersAlive = players.All(player => player.isAlive);
+
+        if (allPlayersAlive)
+        {
+            onWin.Invoke();
+        }
+        else
+        {
+            var allEnemiesDead = enemies.All(enemy => !enemy.isAlive);
+            if (allEnemiesDead)
+            {
+                onGameOver.Invoke();
+            }
+        }
+    }
+}
